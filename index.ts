2d6bb010acb929de49d98f82cd0f1dd7bd65e963
Peter Oti <71887594+PetxCode@users.noreply.github.com>
2023-11-24 19:53:40 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -18,10 +18,11 @@ app.use("/api/test", question);
 app.use("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Welcome" });
 });
-
+// mongodb+srv://shecodesaj:<password>@cluster0.xe1jgnf.mongodb.net/?retryWrites=true&w=majority
+// mongodb+srv://brighterdayscodelab:brighterdayscodelab@cluster0.upn8ipm.mongodb.net/newIntakeDB?retryWrites=true&w=majority
 mongoose
   .connect(
-    "mongodb+srv://brighterdayscodelab:brighterdayscodelab@cluster0.upn8ipm.mongodb.net/newIntakeDB?retryWrites=true&w=majority",
+    "mongodb+srv://shecodesaj:shecodesaj@cluster0.xe1jgnf.mongodb.net/newIntakeDB?retryWrites=true&w=majority",
   )
   .then(() => {
     const server = app.listen(process.env.PORT || port, () => {
